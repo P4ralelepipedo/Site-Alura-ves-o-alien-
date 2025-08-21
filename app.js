b00acccc4003be8ae3ee871ee7eb0af7caad2785
Paralelepípedo <aldry.campos@escola.pr.gov.br>
2025-08-20 22:46:20 -0300

    a

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = ` <div class="cartao__conteudo">
+                    <h3>Alien</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>O que são aliens??</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                       <p>Somos nozes</p>
+                    </div>
+                </div>
+                `
 }
